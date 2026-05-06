0f082dbfec8514113a850dd73ff86e8de94a036a
Arkadii <arkadiychulkov11@gmail.com>
2026-05-06 18:24:20 +0300

    pre added class for endpoints

--- Rate/RateApp/models.py
+++ Rate/RateApp/models.py
@@ -19,8 +19,8 @@ class User(AbstractUser):
     def __str__(self):
         return f"ID: {self.id} {self.username} ({self.email}) - Rating: {self.rating:.2f} based on {self.rated_count} ratings"
     
-
 class Message(models.Model):
+
     message_text = models.TextField()
 
     sender = models.ForeignKey(
@@ -44,3 +44,50 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.sender} -> {self.recipient}"
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_requests")
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_requests")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_accepted = models.BooleanField(default=False)
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ratings")
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="given_ratings")
+    value = models.FloatField()
+
+class Log(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="logs")
+    text = models.TextField()
+
+class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="images")
+    url = models.TextField()
+
+class UserModel:
+    def __init__(self, user_id, username, email):
+        self.id = user_id
+        self.username = username
+        self.email = email
+
+        self.friends = set()
+        self.images = []
+        self.logs = []
+        self.messages = []
+        self.rating = []
+
+class MessageModel:
+    def __init__(self, sender_id, recipient_id, text, send_time=None, is_read=False):
+        self.sender_id = sender_id
+        self.recipient_id = recipient_id
+        self.text = text
+        self.send_time = send_time
+        self.is_read = is_read
+
+class Rate:
+    def __init__(self, value):
+        self.value = value
+
+class Log:
+    def __init__(self, text):
+        self.text = text
